4c2b5697bb4e2b23122a12e967511e9c20a5a334
Om12ek99 <omar.elkamili@gmail.com>
2024-07-18 18:56:14 +0200

    add:footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -13,9 +13,20 @@ export default {
                     <h5 class="text-light">About Us</h5>
                     <p class="text-light">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit, quibusdam
                         veritatis natus aliquam illo in ea ratione! Magnam, optio minima.</p>
-                    <p class="text-light"><i class="fa fa-location"></i> 123 456 897</p>
-                    <p class="text-light"><i class="fa fa-phone"></i> (92) 0325 12344567</p>
+                    <p class="text-light"><i class="fa fa-location"></i> Milano</p>
+                    <p class="text-light"><i class="fa fa-phone"></i> (+39) 351 8545656</p>
                     <p class="text-light"><i class="fa fa-envelope"></i> info@gmail.com</p>
+                </div>
+
+                <div class="col-lg-3">
+                    <h5 class="text-light">Link Veloci</h5>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">In Evidenza</a></li>
+                        <li><a href="#">Servizi</a></li>
+                        <li><a href="#">Recendioni</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
@@ -23,4 +34,13 @@ export default {
 
 </template>
 
-<style></style>
+<style scoped lang="scss">
+    .bg-footer{
+        background-color: #000;
+        padding: 50px 0 30px;
+    }
+    .bg-footer a {
+        text-decoration: none;
+        color: #aeaeae;
+    }
+</style>
