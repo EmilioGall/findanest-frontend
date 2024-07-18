df755e3e07b4cae7da36c7d76cdeea2a15be8b4f
andrea-bertaglia <andreabertaglia@gmail.com>
2024-07-18 16:45:36 +0200

    feat: refactoring components

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
-import AppJumbotron from './components/AppJumbotron.vue';
-import AppMain from './components/AppMain.vue';
-import AppEvidenza from './components/AppEvidenza.vue';
+
 
 export default {
   components: {
 
     AppHeader,
-    AppMain,
-    AppJumbotron,
-    AppEvidenza
 
   },
 
@@ -18,10 +13,8 @@ export default {
 </script>
 
 <template>
+  <AppHeader />
   <router-view></router-view>
-      <AppHeader/>
-      <AppJumbotron/>
-      <AppEvidenza />
 </template>
 
 <style scoped lang="scss"></style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,25 @@
+<script>
+import AppJumbotron from '../components/AppJumbotron.vue';
+import AppEvidenza from '../components/AppEvidenza.vue';
+import AppMain from '../components/AppMain.vue';
+
+export default {
+    components: {
+
+        AppJumbotron,
+        AppEvidenza,
+        AppMain
+
+    },
+
+}
+</script>
+
+<template>
+
+    <AppJumbotron />
+    <AppEvidenza />
+    <AppMain />
+</template>
+
+<style scoped lang="scss"></style>
