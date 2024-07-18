336fccb345009847f929da2a6804199b231a7334
Om12ek99 <omar.elkamili@gmail.com>
2024-07-18 12:51:58 +0200

    add:added section footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,26 +1,26 @@
 <script>
-export default{
+export default {
 
 }
 </script>
 
 <template>
     <!-- Footer -->
-<footer class="bg-dark bg-footer" style="margin-top: 80px;">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-6">
-        <h5 class="text-light">About Us</h5>
-        <p class="text-light">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit, quibusdam veritatis natus aliquam illo in ea ratione! Magnam, optio minima.</p>
-        <p class="text-light"><i class="fa fa-location"></i> 123 456 897</p>
-        <p class="text-light"><i class="fa fa-phone"></i> (92) 0325 12344567</p>
-        <p class="text-light"><i class="fa fa-envelope"></i> info@gmail.com</p>
-      </div>
-    </div>
-  </div>
-</footer>
+    <footer class="bg-dark bg-footer" style="margin-top: 80px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <h5 class="text-light">About Us</h5>
+                    <p class="text-light">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit, quibusdam
+                        veritatis natus aliquam illo in ea ratione! Magnam, optio minima.</p>
+                    <p class="text-light"><i class="fa fa-location"></i> 123 456 897</p>
+                    <p class="text-light"><i class="fa fa-phone"></i> (92) 0325 12344567</p>
+                    <p class="text-light"><i class="fa fa-envelope"></i> info@gmail.com</p>
+                </div>
+            </div>
+        </div>
+    </footer>
 
 </template>
 
-<style>
-</style>
+<style></style>
