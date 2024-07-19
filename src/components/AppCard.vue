70ae43ceddc8fc68ea3bfa0f4de437cf12a7b147
Om12ek99 <omar.elkamili@gmail.com>
2024-07-19 23:41:09 +0200

    add: added axios and cycled to have dynamic posts

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,87 +1,49 @@
 <script>
 export default {
-
+    props: {
+        house: {
+            type: Object,
+            required: true
+        }
+    }
 }
 </script>
 
 <template>
-            <!-- colonna della card -->
-            <div class="col">
-                <div class="card border-0 rounded-0">   <!--Bordi della colonna-->
-                    <img src="/src/real estate/home1.jpg" class="card-img-top rounded-0" alt="img1">
-                    <!-- corpo della card -->
-                    <div class="card-body">
-                        <!-- testo della card -->
-                        <p class="cardtext text-muted">Trento, Italia</p>
+    <!-- colonna della card -->
+    <div class="col">
+        <div class="card border-0 rounded-0">
+            <img :src="house.image" class="card-img-top rounded-0" :alt="house.title">
+            <!-- corpo della card -->
+            <div class="card-body">
+                <!-- testo della card -->
+                <h5 class="cardtext">{{ house.title }}</h5>
+                <p class="cardtext text-muted">{{ house.address }}</p>
 
-                        <!-- caratteristice della card -->
-                        <div class="d-flex justify-content-center mt-3">
-                            <p class="mx-4 text-muted card-text"><i class="fa fa-bed"></i> 3 letti</p>
-                            <p class="mx-4 text-muted card-text"><i class="fa-solid fa-person-shelter"></i> 8 * 8</p>
-                            <p class="mx-4 text-muted card-text"><i class="fa-solid fa-ruler-combined"></i> 2000 mq</p>
-                        </div>
+                <!-- caratteristice della card -->
+                <div class="d-flex justify-content-center mt-3">
+                    <p class="mx-4 text-muted card-text"><i class="fa fa-bed"></i> {{ house.beds }} letti</p>
+                    <p class="mx-4 text-muted card-text"><i class="fa-solid fa-ruler-combined"></i> {{ house.sqm }} mq</p>
+                </div>
 
-                        <!-- bottone della card -->
-                        <div class="d-flex my-2">
-                            <button class="btn btn-lg px-4 rounded custom-color">Prenota ora</button>
-                            <h5 class="my-auto ms-auto">$20,13,048</h5>
-                        </div>
-                    </div>
+                <!-- bottone della card -->
+                <div class="d-flex my-2">
+                    <button class="btn btn-lg px-4 rounded custom-color">Prenota ora</button>
+                    <h5 class="my-auto ms-auto">${{ house.price }}</h5>
                 </div>
             </div>
-            <!-- seconda casa -->
-            <!-- <div class="col">
-                <div class="card border-0 rounded-0">
-                    <img src="/src/real estate/home2.jpg" class="card-img-top rounded-0" alt="img1">
-                    <div class="card-body">
-                        <p class="cardtext text-muted">Trento, Italia</p>
-
-                        <div class="d-flex justify-content-center mt-3">
-                            <p class="mx-4 text-muted card-text"><i class="fa fa-bed"></i> 3 letti</p>
-                            <p class="mx-4 text-muted card-text"><i class="fa-solid fa-person-shelter"></i> 8 * 8</p>
-                            <p class="mx-4 text-muted card-text"><i class="fa-solid fa-ruler-combined"></i> 2000 mq</p>
-                        </div>
-
-                        <div class="d-flex my-2">
-                            <button class="btn btn-lg text-white bg-dark px-4 rounded">Prenota ora</button>
-                            <h5 class="my-auto ms-auto">$20,13,048</h5>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
-
-            <!-- terza casa -->
-            <!-- <div class="col">
-                <div class="card border-0 rounded-0">
-                    <img src="/src/real estate/home3.jpg" class="card-img-top rounded-0" alt="img1">
-                    <div class="card-body">
-                        <p class="cardtext text-muted">Trento, Italia</p>
-
-                        <div class="d-flex justify-content-center mt-3">
-                            <p class="mx-4 text-muted card-text"><i class="fa fa-bed"></i> 3 letti</p>
-                            <p class="mx-4 text-muted card-text"><i class="fa-solid fa-person-shelter"></i> 8 * 8</p>
-                            <p class="mx-4 text-muted card-text"><i class="fa-solid fa-ruler-combined"></i> 2000 mq</p>
-                        </div>
-
-                        <div class="d-flex my-2">
-                            <button class="btn btn-lg text-white bg-dark px-4 rounded">Prenota ora</button>
-                            <h5 class="my-auto ms-auto">$20,13,048</h5>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
-
-            
-
+        </div>
+    </div>
 </template>
 
 <style scoped lang="scss">
 @use "../scss/partials/variables" as *;
-button{
+
+button {
     border: 3px solid $color-light-green;
     transition: background-color 0.3s ease, transform 0.3s ease;
 }
-button:hover{
+button:hover {
     background-color: $color-light-green;
     transform: scale(1.1);
 }
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -7,10 +7,8 @@ import axios from 'axios';
 
 export default {
     components: {
-
         AppCard,
         AppAnnunci
-
     },
 
     data() {
@@ -23,7 +21,6 @@ export default {
             this.homes = resp.data.result;
             console.log(this.homes);
         });
-        
     },
 }
 </script>
@@ -31,7 +28,6 @@ export default {
 <template>
     <!-- container delle cards -->
     <section class="container" style="margin-top: 10px;">
-
         <!-- titolo e descrizione sezione -->
         <p class="pt-5">In Evidenza</p>
         <div class="d-flex">
@@ -42,21 +38,20 @@ export default {
 
         <!-- formattazione delle card -->
         <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 mt-2">
-
-            <!-- sezione dedicata alle cards -->
-            <AppCard />
-            <!-- fine sezione dedicata alle cards -->
+            <div v-for="house in homes" :key="house.id" class="col">
+                <!-- sezione dedicata alle cards -->
+                <AppCard :house="house" />
+                <!-- fine sezione dedicata alle cards -->
+            </div>
         </div>
         <!-- fine formattazione delle cards -->
     </section>
-      <!-- fine container delle cards -->
+    <!-- fine container delle cards -->
 
     <!-- background grigio e logo -->
     <section class="bg-dark" style="margin-top: -15%;">
-
         <!-- contenitore  -->
         <div class="container pt-5">
-
             <!-- riga del fondatore e della suo frase -->
             <div class="row" style="margin-top: 25%;">
                 <!-- sezione delle card annunci non sponsorizzati-->
@@ -64,18 +59,14 @@ export default {
                     <p class="pt-5 text-light">Le nostre proproste</p>
                     <div class="d-flex">
                         <h2 class="customhome">I nostri Alloggi piu recenti</h2>
-                        <h6 class="ms-auto text-light">Scopri <span class="arrow-icon text-light">&rarr;</span>
-                        </h6>
+                        <h6 class="ms-auto text-light">Scopri <span class="arrow-icon text-light">&rarr;</span></h6>
                     </div>
 
                     <!-- formattazione cards -->
                     <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 mt-2">
                         <!-- card degli annunci non sponsorizzati -->
                         <AppAnnunci />
-                         <!-- fine card degli annunci non sponsorizzati -->
-
-
-
+                        <!-- fine card degli annunci non sponsorizzati -->
                     </div>
                     <!-- fine formattazione cards -->
                 </section>
@@ -90,18 +81,11 @@ export default {
 
                 <!-- inizio colonna quotazione -->
                 <div class="col-lg-8 mb-5 mt-5">
-                    <h6 class="lh-lg" style="color: #b5b5b5;">&ldquo; Lorem ipsum dolor, sit amet consectetur
-                        adipisicing elit.
-                        Repudiandae ad, consectetur perspiciatis recusandae quidem iure quia itaque obcaecati
-                        minima
-                        error quae
-                        quaerat ab atque libero est! Eum temporibus dicta voluptatem? &rdquo;</h6>
+                    <h6 class="lh-lg" style="color: #b5b5b5;">&ldquo; Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae ad, consectetur perspiciatis recusandae quidem iure quia itaque obcaecati minima error quae quaerat ab atque libero est! Eum temporibus dicta voluptatem? &rdquo;</h6>
                 </div>
                 <!-- fine colonna quotazione -->
-
             </div>
             <!-- fine riga del fondatore e della suo frase -->
-
 
             <hr class="text-light">
 
@@ -111,28 +95,22 @@ export default {
                     <div class="col-md-3 col-sm-6">
                         <img src="/src/real estate/logo1.png" alt="" style="width: 55%;">
                     </div>
-
                     <div class="col-md-3 col-sm-6">
                         <img src="/src/real estate/logo2.png" alt="" style="width: 55%;">
                     </div>
-
                     <div class="col-md-3 col-sm-6">
                         <img src="/src/real estate/logo3.png" alt="" style="width: 55%;">
                     </div>
-
                     <div class="col-md-3 col-sm-6">
                         <img src="/src/real estate/logo4.png" alt="" style="width: 55%;">
                     </div>
                 </div>
             </div>
             <!-- fine loghi -->
-
         </div>
         <!-- fine contenitore  -->
-
     </section>
     <!-- fine background grigio e logo -->
-
 </template>
 
 <style scoped lang="scss">
