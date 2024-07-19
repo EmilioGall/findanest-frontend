837cb613f8143845eb0987b8d26cfb8ec0965b31
Om12ek99 <omar.elkamili@gmail.com>
2024-07-19 18:38:31 +0200

    add: created AppCard and moved code to it

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,9 +1,82 @@
 <script>
+export default {
+
+}
 </script>
 
 <template>
+    <section class="container" style="margin-top: 100px;">
 
+        <p class="pt-5">In Evidenza</p>
+        <div class="d-flex">
+            <h2>I nostri Alloggi piu Popolari</h2>
+            <h6 class="ms-auto">Scopri <span class="arrow-icon">&rarr;</span></h6>
+        </div>
+
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 mt-2">
+            <div class="col">
+                <div class="card border-0 rounded-0">
+                    <img src="/src/real estate/home1.jpg" class="card-img-top rounded-0" alt="img1">
+                    <div class="card-body">
+                        <p class="cardtext text-muted">Trento, Italia</p>
+
+                        <div class="d-flex justify-content-center mt-3">
+                            <p class="mx-4 text-muted card-text"><i class="fa fa-bed"></i> 3 letti</p>
+                            <p class="mx-4 text-muted card-text"><i class="fa-solid fa-person-shelter"></i> 8 * 8</p>
+                            <p class="mx-4 text-muted card-text"><i class="fa-solid fa-ruler-combined"></i> 2000 mq</p>
+                        </div>
+
+                        <div class="d-flex my-2">
+                            <button class="btn btn-lg text-white bg-dark px-4 rounded">Prenota ora</button>
+                            <h5 class="my-auto ms-auto">$20,13,048</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- seconda casa -->
+            <div class="col">
+                <div class="card border-0 rounded-0">
+                    <img src="/src/real estate/home2.jpg" class="card-img-top rounded-0" alt="img1">
+                    <div class="card-body">
+                        <p class="cardtext text-muted">Trento, Italia</p>
+
+                        <div class="d-flex justify-content-center mt-3">
+                            <p class="mx-4 text-muted card-text"><i class="fa fa-bed"></i> 3 letti</p>
+                            <p class="mx-4 text-muted card-text"><i class="fa-solid fa-person-shelter"></i> 8 * 8</p>
+                            <p class="mx-4 text-muted card-text"><i class="fa-solid fa-ruler-combined"></i> 2000 mq</p>
+                        </div>
+
+                        <div class="d-flex my-2">
+                            <button class="btn btn-lg text-white bg-dark px-4 rounded">Prenota ora</button>
+                            <h5 class="my-auto ms-auto">$20,13,048</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- terza casa -->
+            <div class="col">
+                <div class="card border-0 rounded-0">
+                    <img src="/src/real estate/home3.jpg" class="card-img-top rounded-0" alt="img1">
+                    <div class="card-body">
+                        <p class="cardtext text-muted">Trento, Italia</p>
+
+                        <div class="d-flex justify-content-center mt-3">
+                            <p class="mx-4 text-muted card-text"><i class="fa fa-bed"></i> 3 letti</p>
+                            <p class="mx-4 text-muted card-text"><i class="fa-solid fa-person-shelter"></i> 8 * 8</p>
+                            <p class="mx-4 text-muted card-text"><i class="fa-solid fa-ruler-combined"></i> 2000 mq</p>
+                        </div>
+
+                        <div class="d-flex my-2">
+                            <button class="btn btn-lg text-white bg-dark px-4 rounded">Prenota ora</button>
+                            <h5 class="my-auto ms-auto">$20,13,048</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
 </template>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
