f7e89c6ddb30276c34c76f3b136bd17a7896f6e3
andrea-bertaglia <andreabertaglia@gmail.com>
2024-07-22 10:09:40 +0200

    fix: contact form

--- src/components/AppContactForm.vue
+++ src/components/AppContactForm.vue
@@ -23,6 +23,7 @@ export default {
     },
     methods: {
         submitForm() {
+            // console.log('prova invio');
             this.loading = true;
 
             axios.post('http://127.0.0.1:8000/api/leads', {
@@ -30,75 +31,87 @@ export default {
                 house_id: this.houseObj.id
             })
                 .then((resp) => {
+                    // console.log(resp);
                     this.loading = false;
                     if (resp.data.success) {
                         this.success = true;
                     }
                 })
-                .catch(error => {
-                    this.loading = false;
-                    console.error('Errore nell\'invio del modulo:', error);
-                });
         },
     }
 }
 </script>
 
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-12 col-md-8 col-lg-6">
-                <h5 class="text-center my-4">Vuoi maggiori informazioni?</h5>
+    <div class="container d-flex justify-content-center align-items-center">
+        <div class="col-12 col-md-8">
+            <h5 class="text-center my-4">Vuoi maggiori informazioni?</h5>
+            <form>
                 <div class="card card-border">
                     <!-- Success message -->
                     <div v-if="success" class="success-overlay d-flex flex-column">
                         <i class="fa-regular fa-circle-check"></i>
                         <p class="success-message pt-5 fs-3 fw-bold">Il messaggio è stato inviato con successo!</p>
                     </div>
-                    
+
                     <div class="card-body">
-                        <form @submit.prevent="submitForm">
-                            <div class="row">
-                                <div class="col-12 col-md-6 mb-3">
+                        <div class="row">
+                            <div class="col-6 p-3">
+                                <div class="mb-3">
                                     <label for="name" class="form-label">Nome</label>
-                                    <input type="text" class="form-control input-border" id="name" v-model="formData.name" required>
+                                    <input type="text" class="form-control input-border" id="name"
+                                        aria-describedby="nameHelp" v-model="formData.name">
                                 </div>
-                                <div class="col-12 col-md-6 mb-3">
+                                <div class="mb-3">
                                     <label for="email" class="form-label">Indirizzo E-Mail *</label>
-                                    <input type="email" class="form-control input-border" id="email" v-model="formData.email" required>
+                                    <input type="email" class="form-control input-border" id="email"
+                                        aria-describedby="emailHelp" v-model="formData.email">
                                 </div>
-                                <div class="col-12 col-md-6 mb-3">
+                            </div>
+                            <div class="col-6 p-3">
+                                <div class="mb-3">
                                     <label for="phone_number" class="form-label">Telefono</label>
-                                    <input type="text" class="form-control input-border" id="phone_number" v-model="formData.phone_number">
+                                    <input type="text" class="form-control input-border" id="phone_number"
+                                        aria-describedby="phoneHelp" v-model="formData.phone_number">
                                 </div>
-                                <div class="col-12 col-md-6 mb-3">
-                                    <label class="form-label">Stai richiedendo informazioni per:</label>
-                                    <p class="form-control bg-light">{{ houseObj.title }}</p>
+                                <div class="mb-3">
+                                    <span class="text-secondary form-label">Stai richiedendo informazioni
+                                        per:</span>
+                                    <p class="mt-2 text-secondary form-control bg-light">{{ houseObj.title }}</p>
                                 </div>
-                                <div class="col-12 mb-3">
+                            </div>
+
+                        </div>
+                        <div class="row">
+                            <div class="col-12 pb-3 px-3">
+                                <div class="mb-3">
                                     <label for="message" class="form-label">Messaggio *</label>
-                                    <textarea class="form-control input-border" id="message" v-model="formData.message" required></textarea>
-                                    <div id="messageHelp" class="form-text">Scrivi qui la tua richiesta.</div>
+                                    <textarea class="form-control input-border" id="message"
+                                        v-model="formData.message"></textarea>
+                                    <div id="messageHelp" class="form-text ">Scrivi qui la tua richiesta.</div>
                                 </div>
                             </div>
-                            <div class="d-flex flex-column flex-sm-row justify-content-between align-items-center">
-                                <!-- Send btn -->
-                                <button v-if="!loading" type="submit" class="btn btn-custom mb-2 mb-sm-0">
-                                    <i class="fa-solid fa-paper-plane fa-beat-fade"></i><span class="ms-2">Invia</span>
-                                </button>
-                                <!-- Loading btn -->
-                                <button v-if="loading" class="btn btn-custom" type="button">
-                                    <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
-                                    <span class="ms-2" role="status">Invio del messaggio in corso...</span>
-                                </button>
-                            </div>
-                            <small class="d-block text-center text-secondary mt-2">I campi contrassegnati con * sono obbligatori.</small>
-                        </form>
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <!-- Send btn -->
+                            <button v-if="!loading" @click.prevent="submitForm" type="submit" class="btn btn-custom">
+                                <i class="fa-solid fa-paper-plane fa-beat-fade"></i><span class="ms-2">Invia</span>
+                            </button>
+                            <!-- Loading btn -->
+                            <button v-if="loading" class="btn btn-custom" type="button">
+                                <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
+                                <span class="ms-2" role="status">Invio del messaggio in corso...</span>
+                            </button>
+                            <small class="align-self-end text-secondary">I campi contrassegnati con * sono
+                                obbligatori.</small>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </form>
         </div>
     </div>
+
+
 </template>
 
 <style scoped lang="scss">
@@ -111,8 +124,6 @@ export default {
 
 .btn-custom {
     background-color: $color-light-green;
-    color: white;
-    border: none;
 }
 
 .success-overlay {
@@ -127,14 +138,14 @@ export default {
     justify-content: center;
     align-items: center;
     z-index: 10;
-    text-align: center;
 
     i {
-        font-size: 5rem;
+        font-size: 9rem;
     }
 }
 
 .success-message {
     font-size: 1.25rem;
+    text-align: center;
 }
 </style>
