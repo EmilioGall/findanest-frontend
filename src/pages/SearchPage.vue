4f7a794d20d15858ab572cdc5f656981e074a689
Emilio Gallo <emilio.gallo17@gmail.com>
2024-07-22 23:24:44 +0200

    feat: adjusted visualization card and linked cards to house details - SearchPage.vue

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -6,23 +6,37 @@ import AppIconList from '../components/AppIconList.vue';
 import AppSearchBar from '../components/AppSearchBar.vue';
 
 export default {
+
     components: {
+
         AppIconList,
         AppSearchBar,
         AppCard,
+
     },
 
     data() {
         return {
+
             store,
+
         }
     },
 
     created() {
+
         this.handleSearch();
+
     },
 
     methods: {
+
+        navigateToSinglePage(slug) {
+
+            this.$router.push({ name: 'singlepage', params: { slug } });
+
+        },
+
         async handleSearch() {
             try {
                 const response = await axios.get('http://127.0.0.1:8000/search', {
@@ -34,39 +48,52 @@ export default {
                 this.$router.push("/ricerca")
 
             } catch (error) {
+
                 console.error(error);
+
             }
         },
+
     }
 }
 </script>
 
 <template>
+
+    <!-- Header -->
     <div class="search-header">
+
         <div class="container pt-5">
             <AppSearchBar />
         </div>
+
         <div class="container pt-5">
             <AppIconList />
         </div>
-    </div>
 
-    <div class="results container mt-5">
+    </div>
+    <!-- Header -->
+
+    <!-- Houses Cards Grid -->
+    <div class="results container gap-2 mt-5">
+
         <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 mt-2">
 
-            <AppCard v-for="result in store.results" :key="result.id" :house="result" />
+            <AppCard v-for="result in store.results" :key="result.id" :house="result" @houseSelected="navigateToSinglePage"/>
+
         </div>
+
     </div>
+    <!-- Houses Cards Grid -->
+
 </template>
 
 <style lang="scss" scoped>
+
 .search-header {
+
     background-color: grey;
+
 }
 
-.results {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 1rem;
-}
 </style>
