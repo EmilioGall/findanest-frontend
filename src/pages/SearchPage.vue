2e727703c8de8435b9db272412702c2c5833cbd4
Emilio Gallo <emilio.gallo17@gmail.com>
2024-07-26 01:18:46 +0200

    feat: raw layout - SearchPage.vue

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -29,6 +29,18 @@ export default {
 
     },
 
+    computed: {
+
+        sponsoredHouses() {
+            return this.store.results.filter(house => house.sponsored === 1);
+        },
+
+        houses() {
+            return this.store.results.filter(house => house.sponsored !== 1);
+        },
+
+    },
+
     methods: {
 
         navigateToSinglePage(slug) {
@@ -67,7 +79,8 @@ export default {
 
 <template>
 
-    <main class="pb-5">
+    <main>
+
         <!-- Header -->
         <div class="search-header">
 
@@ -83,20 +96,95 @@ export default {
         <!-- Header -->
 
         <!-- Houses Cards Grid -->
-        <div class="results container gap-2 mt-5">
+        <div class="gap-2">
 
-            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 mt-2">
+            <!-- container delle cards -->
+            <section class="container pt-2" style="margin-top: 10px;">
 
-                <AppCard v-for="result in store.results" :key="result.id" :house="result"
-                    @houseSelected="navigateToSinglePage" />
+                <!-- titolo e descrizione sezione -->
+                <p class="mt-3">In Evidenza</p>
 
-            </div>
+                <div class="d-flex">
+
+                    <h2>I nostri Alloggi più Popolari</h2>
+
+                </div>
+                <!-- fine titolo e descrizione -->
+
+                <!-- formattazione delle card -->
+                <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 mt-2">
+
+                    <div v-for="sponsoredHouse in sponsoredHouses" :key="sponsoredHouse.id" class="col">
+
+                        <!-- sezione dedicata alle cards -->
+                        <AppCard :house="sponsoredHouse" @houseSelected="navigateToSinglePage" class="cardCustom" />
+                        <!-- fine sezione dedicata alle cards -->
+
+                    </div>
+
+                </div>
+                <!-- fine formattazione delle cards -->
+
+            </section>
+            <!-- fine container delle cards -->
+
+            <!-- background grigio e logo -->
+            <section class="bg-dark" style="margin-top: -15%;">
+
+                <!-- contenitore  -->
+                <div class="container pt-2">
+
+                    <!-- riga del fondatore e della suo frase -->
+                    <div class="row" style="margin-top: 25%;">
+
+                        <!-- sezione delle card annunci non sponsorizzati-->
+                        <section class="container cardsnormal">
+
+                            <p class="text-light">Le nostre proposte</p>
+
+                            <div class="d-flex">
+                                <h2 class="customhome">I nostri Alloggi più recenti</h2>
+                                <h6 class="ms-auto text-light">Scopri <span class="arrow-icon text-light">&rarr;</span>
+                                </h6>
+                            </div>
+
+                            <!-- formattazione cards -->
+
+                            <!-- formattazione delle card -->
+                            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 mt-2">
+
+                                <div v-for="house in houses" :key="house.id" class="col">
+
+                                    <!-- sezione dedicata alle cards -->
+                                    <AppCard :house="house" @houseSelected="navigateToSinglePage" class="cardCustom" />
+                                    <!-- fine sezione dedicata alle cards -->
+
+                                </div>
+
+                            </div>
+                            <!-- fine formattazione delle cards -->
+
+                            <!-- fine formattazione cards -->
+
+                        </section>
+                        <!-- fine sezione delle card annunci non sponsorizzati -->
+
+                    </div>
+                    <!-- fine riga del fondatore e della suo frase -->
+
+                </div>
+                <!-- fine contenitore  -->
+
+                <hr class="text-light mt-5 mb-0">
+
+            </section>
+            <!-- fine background grigio e logo -->
 
         </div>
         <!-- Houses Cards Grid -->
+
+
     </main>
-
-
 
 </template>
 
@@ -108,6 +196,19 @@ export default {
 
     background-color: grey;
 
+}
+
+.cardCustom {
+    cursor: pointer;
+}
+
+.arrow-icon {
+    font-size: 28px;
+    font-weight: 800;
+}
+
+.customhome {
+    color: $color-light-green;
 }
 
 main {
