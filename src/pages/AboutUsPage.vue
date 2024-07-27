3fc10520516453dfa1ac1516aa80f3494e425347
danielemantovani <daniele.mantovani124@gmail.com>
2024-07-27 13:15:40 +0200

    fix: responsive about us page

--- src/pages/AboutUsPage.vue
+++ src/pages/AboutUsPage.vue
@@ -8,19 +8,37 @@ export default {
     <div class="container">
         <h1 class="text-center py-5">Chi siamo</h1>
         <div class="row">
-            <div class="col-6">
-                <img src="/src/real estate/Team 7.jpg">
+            <div class="col-md-6 col-12 mb-4 mb-md-0">
+                <img src="/src/real estate/Team 7.jpg" class="img-fluid" alt="Team Image">
             </div>
-            <div class="col-6">
-                <p> <strong>FindNest</strong> è la piattaforma innovativa che ti permette di trovare e prenotare la tua casa ideale in qualsiasi parte del mondo. Che tu stia cercando una breve fuga, un soggiorno prolungato o una nuova residenza temporanea, FindNest ti offre una vasta gamma di opzioni per soddisfare tutte le tue esigenze. <br> Con FindNest, prenotare una casa o un appartamento non è mai stato così facile. La nostra piattaforma intuitiva ti consente di esplorare numerose proprietà e scegliere l'alloggio perfetto per te. Grazie al nostro sistema di pagamento sicuro, puoi effettuare la tua prenotazione con la massima tranquillità. <br> Hai una casa o un appartamento che desideri mettere in affitto? FindNest ti offre l'opportunità di diventare un host e guadagnare un reddito extra. La nostra piattaforma ti guida attraverso ogni passaggio del processo di inserimento, dalla creazione di un annuncio accattivante alla gestione delle prenotazioni. Con FindNest, raggiungere un vasto pubblico di viaggiatori è semplice e immediato. <br><br> Hai domande o bisogno di assistenza? Il nostro team di supporto è sempre pronto ad aiutarti. Contattaci attraverso il nostro servizio clienti 24/7 e ti forniremo tutte le informazioni necessarie per rendere la tua esperienza su FindNest indimenticabile.</p>
+            <div class="col-md-6 col-12">
+                <p>
+                    <strong>FindNest</strong> è la piattaforma innovativa che ti permette di trovare e prenotare la tua
+                    casa ideale in qualsiasi parte del mondo. Che tu stia cercando una breve fuga, un soggiorno
+                    prolungato o una nuova residenza temporanea, FindNest ti offre una vasta gamma di opzioni per
+                    soddisfare tutte le tue esigenze. <br>
+                    Con FindNest, prenotare una casa o un appartamento non è mai stato così facile. La nostra
+                    piattaforma intuitiva ti consente di esplorare numerose proprietà e scegliere l'alloggio perfetto
+                    per te. Grazie al nostro sistema di pagamento sicuro, puoi effettuare la tua prenotazione con la
+                    massima tranquillità. <br>
+                    Hai una casa o un appartamento che desideri mettere in affitto? FindNest ti offre l'opportunità di
+                    diventare un host e guadagnare un reddito extra. La nostra piattaforma ti guida attraverso ogni
+                    passaggio del processo di inserimento, dalla creazione di un annuncio accattivante alla gestione
+                    delle prenotazioni. Con FindNest, raggiungere un vasto pubblico di viaggiatori è semplice e
+                    immediato. <br><br>
+                    Hai domande o bisogno di assistenza? Il nostro team di supporto è sempre pronto ad aiutarti.
+                    Contattaci attraverso il nostro servizio clienti 24/7 e ti forniremo tutte le informazioni
+                    necessarie per rendere la tua esperienza su FindNest indimenticabile.
+                </p>
             </div>
         </div>
-
     </div>
 </template>
 
 <style scoped lang="scss">
-img{
+
+img {
     height: 22rem;
+    object-fit: contain;
 }
 </style>
