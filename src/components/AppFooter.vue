99a7da8cdb7092d19f7b351a6c303cf15781aab4
LucaElisio <personal.elix@gmail.com>
2024-07-30 11:16:26 +0200

    fix: footer links

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -9,27 +9,27 @@ export default {
     <footer class="bg-dark bg-footer">
         <div class="container">
             <div class="row">
+
                 <div class="col-lg-6">
-                    <h5 class="text-light">About Us</h5>
-                    <p class="text-light">Siamo una squadra fortissimi, fatta di gente fantastici.</p>
+                    <h5 class="text-light">Contatti</h5>
+                    <p class="text-light">Il mondo è la tua casa, noi creiamo la porta.</p>
                     <p class="text-light"><i class="fa fa-location"></i> Milano</p>
                     <p class="text-light"><i class="fa fa-phone"></i> (+39) 351 8545656</p>
-                    <p class="text-light"><i class="fa fa-envelope"></i> info@gmail.com</p>
+                    <p class="text-light"><i class="fa fa-envelope"></i> info@findnest.com </p>
                 </div>
 
                 <div class="col-lg-3">
                     <h5 class="text-light">Link Veloci</h5>
                     <ul class="list-unstyled">
                         <li><a href="#">Home</a></li>
-                        <li><a href="#">In Evidenza</a></li>
-                        <li><a href="#">Servizi</a></li>
-                        <li><a href="#">Recendioni</a></li>
-                        <li><a href="#">News</a></li>
+                        <li><a href="/ricerca">Ricerca</a></li>
+                        <li><a href="/chi-siamo">Chi siamo</a></li>
+                        <li><a href="http://127.0.0.1:8000">Pubblica il tuo annuncio</a></li>
                     </ul>
                 </div>
 
                 <div class="col-lg-3">
-                    <h5 class="text-light">Stai Connesso</h5>
+                    <h5 class="text-light">I nostri social</h5>
                     <ul class="list-unstyled">
                         <li><a href="#"><i class="fab fa-facebook"></i> Faceboook</a></li>
                         <li><a href="#"><i class="fab fa-linkedin"></i> Linedin</a></li>
@@ -37,6 +37,7 @@ export default {
                         <li><a href="#"><i class="fab fa-twitter"></i> Twitter</a></li>
                     </ul>
                 </div>
+
             </div>
 
             <hr>
@@ -44,7 +45,7 @@ export default {
             <div class="row">
                 <div class="col-lg-12">
                     <p class="text-center text-light">
-                        Copyright &copy; 2024 <a href="https://boolean.co.uk/">Boolean</a>. All Rights Reserved.
+                        Copyright &copy; 2024 <a href="https://boolean.co.uk/"> FindNest </a>. All Rights Reserved.
                     </p>
                 </div>
             </div>
